c3557d112fa762598c001411f523582fbba56bc1
zioroboco <zioroboco@gmail.com>
2018-05-26 17:36:24 +1000

    Implement handler function returning S3 data

--- packages/services/src/handler.spec.ts
+++ packages/services/src/handler.spec.ts
@@ -1,15 +1,18 @@
 import { Handler, Context, Callback, APIGatewayEvent } from "aws-lambda"
-import { makeHandler } from "./handler"
+import { response, createHandler, keyFromPath } from "./handler"
+import { createS3DataRetriever, DataRetriever } from "./data"
 
-beforeEach(() => jest.resetAllMocks())
+const pathKeyPairs = [
+  { path: "", key: "index.json" },
+  { path: "/", key: "index.json" },
+  { path: "/1", key: "1.json" },
+  { path: "/2", key: "2.json" },
+  { path: "/1.json", key: "1.json" },
+  { path: "/2.json", key: "2.json" }
+]
 
-describe("handler function", () => {
-  const handler = makeHandler()
-  const event = {} as APIGatewayEvent
-  const context = {} as Context
-  const callback = jest.fn<Callback>()
-
-  it("runs without error", () => {
-    handler(event, context, callback)
+describe("keyFromPath function", () => {
+  it("maps paths to correct keys", () => {
+    pathKeyPairs.forEach(({ path, key }) => expect(keyFromPath(path)).toBe(key))
   })
 })
--- packages/services/src/handler.ts
+++ packages/services/src/handler.ts
@@ -1,16 +1,41 @@
-import { Handler } from "aws-lambda"
+import { Handler, APIGatewayEvent } from "aws-lambda"
+import { DataRetriever, createS3DataRetriever } from "./data"
 
-type HandlerFactory = () => Handler
-
-const makeHandler: HandlerFactory = () => (event, context, callback) => {
-  const response = {
-    statusCode: 200,
-    headers: { "Content-Type": "text/plain" },
-    body: "OK"
-  }
-  callback(null, response)
+/** Return the key of the target JSON file for a given path. */
+const keyFromPath = (path: string) => {
+  const match = path.match(/^\/(\d+)/)
+  return `${match && match[1] ? match[1] : "index"}.json`
 }
 
-const handler = makeHandler()
+/** Construct an HTTP response object. */
+const response = (status: number, data?: string) => ({
+  statusCode: status,
+  headers: {
+    "Access-Control-Allow-Origin": "*",
+    "Content-Type": "application/json"
+  },
+  body: data || ""
+})
 
-export { handler, makeHandler }
+type HandlerFactory = (getData: DataRetriever) => Handler
+
+/** Return a handler function constructed with the passed data retriever. */
+const createHandler: HandlerFactory = getData => (
+  event: APIGatewayEvent,
+  context,
+  callback
+) => {
+  const key = keyFromPath(event.path)
+  getData(key)
+    .then(data => callback(null, response(200, data)))
+    .catch(error => callback(null, response(404)))
+}
+
+// Variables passed from Lambda execution context
+const AMPED_DATA_BUCKET = process.env.AMPED_DATA_BUCKET!
+const SLS_REGION = process.env.SLS_REGION!
+
+const s3dataRetriever = createS3DataRetriever(AMPED_DATA_BUCKET, SLS_REGION)
+const handler = createHandler(s3dataRetriever)
+
+export { keyFromPath, response, handler, createHandler }
