d4e57c51ba76d6176e49ea4924f2a788cfd96a9a
zioroboco <zioroboco@gmail.com>
2018-05-27 19:35:24 +1000

    Add mock event logger to storybook

--- packages/components/stories/SurveyList.stories.tsx
+++ packages/components/stories/SurveyList.stories.tsx
@@ -1,5 +1,6 @@
 import * as React from "react"
 import { storiesOf } from "@storybook/react"
+import { action } from "@storybook/addon-actions"
 import { SurveyListProps } from "@amped/types"
 import { SurveyList } from "@amped/components"
 
