20e3b71a830f9a501f3d006c104777c053c1088c
zioroboco <zioroboco@gmail.com>
2018-05-27 20:52:36 +1000

    Create list of summary elements

--- packages/components/src/SurveyList/SurveyList.tsx
+++ packages/components/src/SurveyList/SurveyList.tsx
@@ -1,8 +1,42 @@
 import * as React from "react"
-import { SurveyListProps } from "@amped/types"
+import { SurveyListProps, SurveyResultList, SurveyResult } from "@amped/types"
+import {
+  Typography,
+  CircularProgress,
+  ExpansionPanel,
+  ExpansionPanelSummary,
+  ExpansionPanelDetails
+} from "@material-ui/core"
+import * as styles from "./SurveyList.css"
 
+/** An individual survey result summary and optional expanded detail. */
+const SurveyElement = ({ result, index }) => {
+  return (
+    <ExpansionPanel key={index}>
+      <ExpansionPanelSummary>
+        <Typography>{result.name}</Typography>
+      </ExpansionPanelSummary>
+    </ExpansionPanel>
+  )
+}
+
+/** A list of survey result summaries, expandable with additional detail. */
 const SurveyList = (props: SurveyListProps) => {
-  return <div />
+  const { state, handleRequestDetailAtIndex } = props
+  if (state.summary) {
+    const surveyResults = state.summary.survey_results
+    const surveyElement = surveyResults.map((result, i) => (
+      // Indices start at 1, for consistency with the API data
+      <SurveyElement result={result} index={i + 1} key={i} />
+    ))
+    return <div className={styles.root}>{surveyElement}</div>
+  } else {
+    return (
+      <div className={styles.spinner}>
+        <CircularProgress />
+      </div>
+    )
+  }
 }
 
 export { SurveyList }
--- packages/components/stories/SurveyList.stories.tsx
+++ packages/components/stories/SurveyList.stories.tsx
@@ -1,17 +1,45 @@
 import * as React from "react"
 import { storiesOf } from "@storybook/react"
 import { action } from "@storybook/addon-actions"
-import { SurveyListProps } from "@amped/types"
+import { SurveyListProps, SurveyResultList } from "@amped/types"
 import { SurveyList } from "@amped/components"
 
-const baseProps: SurveyListProps = {
-  state: {
-    summary: undefined,
-    detail: {}
-  },
+const props: SurveyListProps = {
+  state: { summary: undefined, detail: {} },
   handleRequestDetailAtIndex: () => null
 }
 
-storiesOf("SurveyList", module).add("Basic", () => {
-  return <SurveyList {...baseProps} />
-})
+const summary: SurveyResultList = {
+  survey_results: [
+    {
+      name: "Basic Existentialism",
+      participant_count: 1,
+      response_rate: 1,
+      url: ""
+    },
+    {
+      name: "Deeper Cosmic Futility",
+      participant_count: 1,
+      response_rate: 1,
+      url: ""
+    }
+  ]
+}
+
+storiesOf("SurveyList", module)
+  .add("Loading...", () => {
+    const props: SurveyListProps = {
+      state: { summary: undefined, detail: {} },
+      handleRequestDetailAtIndex: () => null
+    }
+    return <SurveyList {...props} />
+  })
+
+  .add("With Summaries", () => {
+    return (
+      <SurveyList
+        state={{ detail: {}, summary }}
+        handleRequestDetailAtIndex={() => null}
+      />
+    )
+  })
