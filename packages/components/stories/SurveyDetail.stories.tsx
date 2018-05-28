e97330e044d199e7d1ca89a9b2db3a69d89087f7
zioroboco <zioroboco@gmail.com>
2018-05-28 10:45:17 +1000

    Add story for detail component with data

--- packages/components/stories/SurveyDetail.stories.tsx
+++ packages/components/stories/SurveyDetail.stories.tsx
@@ -4,15 +4,92 @@ import { SurveyResultDetail } from "@amped/types"
 import { SurveyDetail, SurveyDetailProps } from "@amped/components"
 
 storiesOf("SurveyDetail", module)
-  .add("No Data", () => {
+  .add("Loading...", () => {
     const props: SurveyDetailProps = {
-      detail: undefined
+      detail: {}
     }
     return <SurveyDetail {...props} />
   })
-  .add("Data Pending", () => {
+
+  .add("With Detail", () => {
     const props: SurveyDetailProps = {
-      detail: {}
+      detail: {
+        survey_result_detail: {
+          name: "Basic Existentialism",
+          participant_count: 2,
+          response_rate: 0.5,
+          submitted_response_count: 4,
+          url: "",
+          themes: [
+            {
+              name: "Subconscious",
+              questions: [
+                {
+                  description:
+                    "Do your episodes of non-specific malaise affect your work?",
+                  question_type: "ratingquestion",
+                  survey_responses: [
+                    {
+                      id: 1,
+                      question_id: 1,
+                      respondent_id: 1,
+                      response_content: "4"
+                    },
+                    {
+                      id: 2,
+                      question_id: 1,
+                      respondent_id: 2,
+                      response_content: "3"
+                    }
+                  ]
+                },
+                {
+                  description:
+                    "How frequent are your indescribable night terrors?",
+                  question_type: "ratingquestion",
+                  survey_responses: [
+                    {
+                      id: 3,
+                      question_id: 2,
+                      respondent_id: 1,
+                      response_content: "5"
+                    },
+                    {
+                      id: 4,
+                      question_id: 2,
+                      respondent_id: 2,
+                      response_content: ""
+                    }
+                  ]
+                }
+              ]
+            },
+            {
+              name: "Conscious",
+              questions: [
+                {
+                  description: "Well? Are you?",
+                  question_type: "ratingquestion",
+                  survey_responses: [
+                    {
+                      id: 5,
+                      question_id: 3,
+                      respondent_id: 1,
+                      response_content: ""
+                    },
+                    {
+                      id: 2,
+                      question_id: 3,
+                      respondent_id: 2,
+                      response_content: ""
+                    }
+                  ]
+                }
+              ]
+            }
+          ]
+        }
+      }
     }
     return <SurveyDetail {...props} />
   })
