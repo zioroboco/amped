c198a7b2744efb932baca2ef6fc1f34c9819763c
zioroboco <zioroboco@gmail.com>
2018-06-12 22:07:37 +1000

    Add alternate sort methods to frequency lists

--- packages/components/src/SurveyDetail/ResponseFrequencies.tsx
+++ packages/components/src/SurveyDetail/ResponseFrequencies.tsx
@@ -2,6 +2,7 @@ import * as React from "react"
 import * as styles from "./SurveyDetail.css"
 import { FrequencyMap } from "./SurveyDetail"
 import { toPairs } from "ramda"
+import { Button } from "@material-ui/core"
 
 /** The percentage of total responses which were in a particular category. */
 type PercentageOfResponses = React.SFC<{ count: number; total: number }>
@@ -46,29 +47,74 @@ const FrequencyListItem: FrequencyListItem = ({
   )
 }
 
-/** A list of the frequencies at which different responses were recorded. */
-type ResponseFrequencies = React.SFC<{
+type ResponseFrequenciesProps = {
   frequencies: FrequencyMap
   totalResponseCount: number
-}>
-
-const ResponseFrequencies: ResponseFrequencies = ({
-  frequencies,
-  totalResponseCount
-}) => {
-  const frequencyListItems = toPairs(frequencies)
-    // warning: relies on JS coercion of string arguments to the < operator
-    .sort(([catA], [catB]) => (catA < catB ? 1 : -1))
-    .filter(([category, count]) => count > 0)
-    .map(([category, count], i) => (
-      <FrequencyListItem
-        responseCategory={category}
-        responseCount={count}
-        totalResponseCount={totalResponseCount}
-        key={i}
-      />
-    ))
-  return <ul>{frequencyListItems}</ul>
 }
+
+enum SortMethod {
+  category = "category",
+  count = "count"
+}
+
+type ResponseFrequenciesState = {
+  sortMethod: SortMethod
+}
+
+/** A list of the frequencies at which different responses were recorded. */
+class ResponseFrequencies extends React.Component<
+  ResponseFrequenciesProps,
+  ResponseFrequenciesState
+> {
+  constructor(props: ResponseFrequenciesProps) {
+    super(props)
+    this.state = {
+      sortMethod: SortMethod.category
+    }
+  }
+
+  toggleSortMethod = () =>
+    this.setState({
+      sortMethod:
+        this.state.sortMethod === SortMethod.count
+          ? SortMethod.category
+          : SortMethod.count
+    })
+
+  render(): JSX.Element {
+    const frequencyListItems = toPairs(this.props.frequencies)
+      .sort(
+        this.state.sortMethod === SortMethod.count
+          ? sortByCount
+          : sortByCategory
+      )
+      .filter(([category, count]) => count > 0)
+      .map(([category, count], i) => (
+        <FrequencyListItem
+          responseCategory={category}
+          responseCount={count}
+          totalResponseCount={this.props.totalResponseCount}
+          key={i}
+        />
+      ))
+
+    return (
+      <div>
+        <Button
+          className={styles.toggleSort}
+          onClick={this.toggleSortMethod}
+        >{`Sort by response ${
+          this.state.sortMethod === SortMethod.count
+            ? SortMethod.category
+            : SortMethod.count
+        }`}</Button>
+        <ul>{frequencyListItems}</ul>
+      </div>
+    )
+  }
+}
+
+const sortByCategory = (a, b) => (b[0] < a[0] ? -1 : 1)
+const sortByCount = (a, b) => (b[1] < a[1] ? -1 : 1)
 
 export { ResponseFrequencies }
